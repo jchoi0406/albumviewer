1714c289ab977e8865abe98062ff3a925d51cf5d
jchoi0406 <jiyongboy@gmail.com>
2023-07-17 20:48:36 +1200

    aaaaa

---
+++ frontend/src/pages/components/AlbumViewSearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from 'react';
+export default function AlbumViewSearchBar(props) {
+  function handleChange(event){
+    props.setSearchAlbumName(event.target.value);
+  }
+  return (
+    <div>
+        <input className="album-page-search" onChange={handleChange} name="search" placeholder="Search for an album..."></input>
+    </div>
+  )
+}
+
