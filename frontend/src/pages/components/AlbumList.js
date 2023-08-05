373a1a941e8d7359e9ce65b378d9afaad331e6cc
jchoi0406 <jiyongboy@gmail.com>
2023-08-05 22:58:47 +1200

    fixed dispaly not working properly and started work on moving the update/delete on a sidebar which will appear when the user clicks the 3 icons for more options

--- frontend/src/pages/components/AlbumList.js
+++ frontend/src/pages/components/AlbumList.js
@@ -1,10 +1,12 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export default function AlbumList(props){
     const albumsMapped = props.albums.map((album, index)=>{
         const normalStyle = {
             display: 'flex',
             flexDirection: 'row',
+            justifyContent: "space-between",
             wordWrap: 'break-word',
             border: '2px solid #CCCCCC',
             borderRadius: '10px',
@@ -14,25 +16,35 @@ export default function AlbumList(props){
             cursor: 'pointer',
         }
         const selectedStyle = {...normalStyle, border: "2px solid black"} 
-        
+        function optionClicked(event){
+            console.log("clicked!")
+        }
         return (
-        <div className="album" style={(index === props.selectedAlbum) ? selectedStyle: normalStyle} key={album.id} onClick={()=>props.setSelectedAlbum(index)}>
-            <div className="album-container">
-                <div className="album-left">
-                    <img className="album-img" src={album.albumCover} alt=''/>
-                    <p className="album-rating">{album.albumRating}/10</p>
-                    {album.dateAdded.slice(0,10)}
-                </div>
-                <div className="album-info-container">
-                    <div className="album-info">
-                        <h2 className="album-title">{album.albumName}</h2>
-                        <h3 className="album-artist">{album.albumArtist}</h3>
-                        <p className="album-review">{album.albumReview}</p>
+        <div className="album-display" style={(index === props.selectedAlbum) ? selectedStyle: normalStyle} key={album.id} /*onClick={()=>props.setSelectedAlbum(index)}*/>
+            <div className="album">
+                <div className="album-container">
+                    <div className="album-left">
+                        <img className="album-img" src={album.albumCover} alt=''/>
+                        <p className="album-rating">{album.albumRating}/10</p>
+                        {album.dateAdded.slice(0,10)}
+                    </div>
+                    <div className="album-info-container">
+                        <div className="album-info">
+                            <h2 className="album-title">{album.albumName}</h2>
+                            <h3 className="album-artist">{album.albumArtist}</h3>
+                            <p className="album-review">{album.albumReview}</p>
+                        </div>
                     </div>
                 </div>
-                <div className="album-options"></div>   
             </div>
+            <div className="album-options" onClick={optionClicked}>
+                <button className="options"><FontAwesomeIcon icon="fa-solid fa-ellipsis" /></button>
+                <div className="dropdown-menu">
+                    Dropdown Content
+                </div>
+            </div>   
         </div>
+
         )})
     return albumsMapped;
 }
